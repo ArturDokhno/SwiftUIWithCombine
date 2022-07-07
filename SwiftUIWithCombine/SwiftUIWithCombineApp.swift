2a9c480bf76372f3e392995a8baed864ce7883ea
ArturDokhno <artur.dohno@icloud.com>
2022-07-07 12:08:50 +0500

    Initial Commit

---
+++ SwiftUIWithCombine/SwiftUIWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithCombineApp.swift
+//  SwiftUIWithCombine
+//
+//  Created by Артур Дохно on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
